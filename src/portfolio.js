4ebce015908054ec0e787733926fb0aca975fc19
Nick's Mac <D3-MBP-0070-K4W0N4TW01@D3-MBP-0070-K4W0N4TW01.local>
2024-02-03 00:03:45 +0800

    feat: update portfolio contents

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,49 +1,32 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://nick-oyk.github.io',
+  title: 'NOYK.'
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Nicholas Ooi Yu Khai',
+  role: 'Software Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+    'I am a software engineer with knowledgeable skills in modern-day technologies and experience using Frontend and Backend Technology stacks, focusing on Nodejs and Reactjs.',
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
-  },
+    linkedin: 'https://www.linkedin.com/in/ooi-yu-khai-3a1930193/',
+    github: 'https://github.com/Nick-Oyk'
+  }
 }
 
 const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Setel EV Charging',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 2',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 3',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
+      'The production-ready Backend EV Charging service enables users to charge with multiple vendors, including JomCharge and ChargeNGo, through the Setel app. Built on Nest JS, it leverages MongoDB as its database and is deployed on AWS using Kubernetes and ArgoCD for efficient management and scalability. This comprehensive solution seamlessly integrates various charging vendors, providing users with a convenient and reliable platform for EV charging needs. With robust technology stack and deployment infrastructure, it ensures optimal performance and scalability for evolving user demands in the electric vehicle charging ecosystem.      ',
+    stack: ['NestJs', 'React', 'MongoDB', 'AWS'],
+    livePreview: 'https://www.setel.com/ev-charging'
+  }
 ]
 
 const skills = [
@@ -54,18 +37,16 @@ const skills = [
   'JavaScript',
   'TypeScript',
   'React',
-  'Redux',
-  'SASS',
-  'Material UI',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'AWS',
+  'Kubernetes',
+  'Gitops',
+  'NestJs'
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'nicholasoyk@gmail.com'
 }
 
 export { header, about, projects, skills, contact }
